c91f41f9642df8435c75aad2430e012daddb0626
WQ <wingquan.chang@gmail.com>
2025-10-22 17:35:59 +0000

    remove auth verifier lambda

--- localmock/deploy-local.sh
+++ localmock/deploy-local.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 CUR_DIR="$(pwd)"
-source "$CUR_DIR"/build.sh
 # cd "$CUR_DIR"/terraform/aws
 # tflocal destroy -auto-approve && tflocal apply -auto-approve
 cd "$CUR_DIR"/terraform/keycloak
 tflocal apply -var-file="secrets.tfvars" -auto-approve
-cd "$CUR_DIR"/terraform/nats-js
+cd "$CUR_DIR"/terraform/nats
 tflocal apply -auto-approve
 cd "$CUR_DIR"
 echo "✓ Terraform configurations applied successfully"
